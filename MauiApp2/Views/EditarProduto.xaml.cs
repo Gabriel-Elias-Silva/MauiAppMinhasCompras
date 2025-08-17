823adae424eb0ab81af809fe43fc5e5c34203d6a
Elias B. Altas <Elias B. Altas@DESKTOP-V8SSBVJ>
2025-08-16 21:53:54 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp2/Views/EditarProduto.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp2.Views;
+
+public partial class EditarProduto : ContentPage
+{
+	public EditarProduto()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiApp2/Views/ListaProduto.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp2.Views;
+
+public partial class ListaProduto : ContentPage
+{
+	public ListaProduto()
+	{
+		InitializeComponent();
+	}
+}
