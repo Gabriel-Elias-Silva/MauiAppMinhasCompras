0c9e49086bc907356c7ac4870cc7d0fd077314aa
Elias B. Altas <Elias B. Altas@DESKTOP-V8SSBVJ>
2025-08-17 21:57:43 -0300

    Construção da classe para fazer CRUD no SQLite

--- MauiApp2/Helpers/SQLiteDatabaseHeper.cs
+++ MauiApp2/Helpers/SQLiteDatabaseHeper.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using MauiApp2.Models;
+using SQLite;
 namespace MauiApp2.Helpers
 {
-    internal class SQLiteDatabaseHeper
+    public class SQLiteDatabaseHeper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHeper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+
+        }
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE id=? ";
+            return _conn.QueryAsync<Produto>(sql);
+        }
+
+        public Task<int> Delete(int id)
+        {
+
+           return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+        public Task <List<Produto>> GetAll()
+        {
+
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * Produto SET WHERE descricao LIKE '%" + q + "%' ";
+            return _conn.QueryAsync<Produto>(sql);
+
+        }
+
     }
 }
